58f7744092d6f8dd236b8dbc68ffd69add50b1b3
cyalater00 <Jachimikeonwuka@gmail.comgit config --global user.name  JACHIMIKE ONWUKAgit config --global user.email Jachimikeonwuka@gmail.com>
2024-10-04 10:15:00 -0400

    todoupdate

---
+++ src/TodoContent.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import ActionButton from "./ActionButton";
+import "./TodoContent.css";
+
+export default function TodoContent({ javascriptPass, todosDispatch }) {
+  const { title, completed, id } = javascriptPass;
+  console.log(javascriptPass)
+  const [status, setState]=useState(completed)
+  return (
+    <div className="container">
+      <input type="checkbox"  checked={status} onChange= {(e) => setState(e.target.checked) }/>
+      <p>{title}</p>
+      <button>Edit</button>
+      <button
+        onClick={() => todosDispatch({ type: "delete-todo", payload: { id } })}
+      >
+        Delete
+      </button>
+    </div>
+  );
+}
